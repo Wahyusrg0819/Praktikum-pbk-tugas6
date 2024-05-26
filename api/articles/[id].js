471646e6a8d5b71dc47e7694a2096e78ad2ef277
Wahyusrg0819 <fatina0819@gmail.com>
2024-05-26 11:12:40 +0700

    fixing

--- api/articles/[id].js
+++ api/articles/[id].js
@@ -1,9 +1,9 @@
-const { createServer } = require('json-server');
+import { createServer } from 'json-server';
 const server = createServer();
-const router = server.router('db.json'); // Path to db.json
+const router = server.router('../../db.json'); // Path to db.json
 const middlewares = server.defaults();
 
 server.use(middlewares);
 server.use('/api/articles/:id', router);
 
-module.exports = server;
+export default server;
